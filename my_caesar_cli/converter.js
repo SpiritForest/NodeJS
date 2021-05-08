654a9bbcff6489253d78b9bc20250b0485d0bfd3
dzakhar@adnoc.ae <dzakhar@adnoc.ae>
2021-05-09 02:59:45 +0300

    feat: add the Transform function

--- my_caesar_cli/converter.js
+++ my_caesar_cli/converter.js
@@ -1,64 +1,60 @@
 const alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
 ];
 
 function getShiftedChar(sChar, iShift) {
-    let iShiftAbs = Math.abs(iShift);
-    let iNewCharIndex;
-    let aAlphabetReversed;
-    let bUpperCase;
-    let iCharIndex;
-    let aArray;
+  const iShiftAbs = Math.abs(iShift);
+  const bUpperCase = !!/[A-Z]/.test(sChar);
+  let aAlphabetReversed;
+  let iCharIndex;
 
-    if (/[^a-zA-Z]/.test(sChar) || iCharIndex === -1) {
-        return sChar;
-    }
+  if (/[^a-zA-Z]/.test(sChar) || iCharIndex === -1) {
+    return sChar;
+  }
 
-    bUpperCase = /[A-Z]/.test(sChar) ? true : false;
+  if (iShift < 0) {
+    aAlphabetReversed = alphabet.slice().reverse();
+  }
 
-    if (iShift < 0) {
-        aAlphabetReversed = alphabet.slice().reverse();
-    }
+  const aArray = Array.isArray(aAlphabetReversed) ? aAlphabetReversed : alphabet;
+  iCharIndex = aArray.indexOf(sChar.toLowerCase());
+  const iNewCharIndex = (iCharIndex + iShiftAbs) % aArray.length;
 
-    aArray = Array.isArray(aAlphabetReversed) ? aAlphabetReversed : alphabet;
-    iCharIndex = aArray.indexOf(sChar.toLowerCase());
-    iNewCharIndex = (iCharIndex + iShiftAbs) % aArray.length;
-
-    return bUpperCase ? aArray[iNewCharIndex].toUpperCase() : aArray[iNewCharIndex];
-};
+  return bUpperCase ? aArray[iNewCharIndex].toUpperCase() : aArray[iNewCharIndex];
+}
 
 module.exports = function convertString(sString, bEncrypt, iShift) {
-    const aResult = [];
-    let iResultShifting = bEncrypt ? iShift : -iShift;
+  const aResult = [];
+  const iResultShifting = bEncrypt ? iShift : -iShift;
 
-    for (let key of sString) {
-        aResult.push(getShiftedChar(key, iResultShifting))
-    }
+  [].forEach.call(sString, (sChar) => {
+    aResult.push(getShiftedChar(sChar, iResultShifting));
+  });
 
-    return aResult.join("");
+  return aResult.join('');
 };
--- my_caesar_cli/my_caesar_cli.js
+++ my_caesar_cli/my_caesar_cli.js
@@ -1,44 +1,85 @@
-const { Command } = require('commander');
+const {
+  Transform,
+} = require('stream');
+const {
+  pipeline,
+} = require('stream');
+const {
+  Command,
+} = require('commander');
+const process = require('process');
 const fs = require('fs');
 const converter = require('./converter');
-const program = new Command();
-program.version('0.0.1');
 
-function getOptions () {
+const encodingType = 'utf8';
+
+function getOptions() {
+  const program = new Command();
+
+  program.version('0.0.1');
   program
-  .option('-s, --shift <number>', 'shift')
-  .option('-i, --input <string>', 'input')
-  .option('-o, --output <string>', 'output')
-  .option('-a, --action <type>', 'action');
-  
+    .option('-s, --shift <number>', 'shift')
+    .option('-i, --input <string>', 'input')
+    .option('-o, --output <string>', 'output')
+    .option('-a, --action <type>', 'action');
+
   program.parse(process.argv);
-  
+
   return program.opts();
+}
+
+function transformChunk(chunk) {
+  const options = getOptions();
+  const bEncrypt = options.action === 'encode';
+
+  if (options.shift && options.action && ['encode', 'decode'].includes(options.action)) {
+    return converter(chunk, bEncrypt, options.shift);
+  }
+
+  process.stderr.write('The process should exit with non-zero status code');
+  return undefined;
 }
 
 function modifyData() {
   const options = getOptions();
-  let readableStream = options.input ? fs.createReadStream(options.input, "utf8") : process.stdin("utf8");
-  let writeableStream = options.output ? fs.createWriteStream(options.output) : process.stdout;
-  
-  readableStream.on("data", (chunk) => {
-    console.log(chunk)
-    let transformedChunk = transformChunk(chunk);
 
-    writeableStream.write(transformedChunk);
+  const readableStream = options.input ? fs.createReadStream(options.input) : process.stdin;
+  const writeableStream = options.output ? fs.createWriteStream(options.output) : process.stdout;
+  const transformStream = new Transform({
+    transform(chunk, encoding, encode) {
+      const sChunk = encoding === 'buffer' ? chunk.toString(encodingType) : chunk;
+      const sTransformedChunk = transformChunk(sChunk);
+
+      encode(null, sTransformedChunk);
+    },
   });
-}  
 
+  readableStream.on('error', (err) => {
+    process.stderr.write(`Input file doesn't exist: ${err.message}`);
+  });
 
-function transformChunk (chunk) {
-  const options = getOptions();
-  const bEncrypt = options.action === "encode";
+  writeableStream.on('error', (err) => {
+    process.stderr.write(`Output file doesn't exist: ${err.message}`);
+  });
 
-  if (options.shift && options.action && ["encode", "decode"].includes(options.action)) {
-    return converter(chunk, bEncrypt, options.shift);
-  } else {
-    process.strerr("The process should exit with non-zero status code");
-  }
+  transformStream.on('error', (err) => {
+    process.stderr.write(`An error occured while data transformation: ${err.message}`);
+  });
+
+  readableStream.setEncoding(encodingType);
+
+  pipeline(
+    readableStream,
+    transformStream,
+    writeableStream,
+    (err) => {
+      if (err) {
+        process.stderr.write('Pipeline failed.', err);
+      } else {
+        process.stderr.write('Pipeline succeeded.');
+      }
+    },
+  );
 }
 
 modifyData();
